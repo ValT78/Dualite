0d510b815952c7a44d7dbe893a09777c5b757d1f
ValT78 <118465560+ValT78@users.noreply.github.com>
2025-03-23 15:38:02 +0100

    Merge branch 'main' of https://github.com/ValT78/Dualite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,18 +10,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (GameManager.Instance == null) Instance = this;
+        if (Instance == null) Instance = this;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/Scripts/UI/Handshake.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Handshake : MonoBehaviour
+{
+    [SerializeField] private float angleMin = -10f; // Angle minimum d'oscillation
+    [SerializeField] private float angleMax = 10f;  // Angle maximum d'oscillation
+
+    private float timeOffset;
+
+    private void Start()
+    {
+        // Décale l'animation pour éviter une synchronisation parfaite avec d'autres objets
+        timeOffset = Random.Range(0f, Mathf.PI * 2f);
+    }
+
+    private void Update()
+    {
+        // Calcule l'angle basé sur un sinus oscillant entre -1 et 1
+        float angle = Mathf.Lerp(angleMin, angleMax, (Mathf.Sin((Time.time + timeOffset) * GameManager.Instance.anger * Mathf.PI * 4) + 1f) / 2f);
+
+        // Applique la rotation
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
